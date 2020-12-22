ff36662f0de7804b7a2d3ca7c86e93e4bf8f3fd4
grisw <514335188@qq.com>
2020-12-22 11:08:35 +0800

    CompressedImage Camera.

--- src/camera/camera/node.py
+++ src/camera/camera/node.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 import cv2
 from cv_bridge import CvBridge
 from rclpy.qos import QoSProfile
@@ -10,23 +10,29 @@ class CameraPublisher(Node):
 
     def __init__(self):
         super().__init__('camera_publisher')
-        self.publisher_ = self.create_publisher(Image, 'camera', QoSProfile(depth=100))
+        self.publisher_ = self.create_publisher(CompressedImage, 'camera', QoSProfile(depth=100))
         self.cap = cv2.VideoCapture(0)
         self.br = CvBridge()
+        self.timer = self.create_timer(1 / 60, self.poll)
 
-    def loop(self):
+    def poll(self):
         ret, frame = self.cap.read()
-        msg = self.br.cv2_to_imgmsg(frame)  # Convert the image to a message
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % frame.size)
+        if ret:
+            msg = self.br.cv2_to_compressed_imgmsg(frame, 'jpg')  # Convert the image to a message
+            self.publisher_.publish(msg)
+            self.get_logger().info('Publishing: "%s"' % frame.size)
+
+    def close(self):
+        self.cap.release()
 
 
 def main(args=None):
     rclpy.init(args=args)
     camera_publisher = CameraPublisher()
     try:
-        camera_publisher.loop()
+        rclpy.spin(camera_publisher)
     finally:
+        camera_publisher.close()
         camera_publisher.destroy_node()
         rclpy.shutdown()
 
