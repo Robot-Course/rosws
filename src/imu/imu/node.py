563cbaff4d99deb3dc1c230860235ee0a41421e3
grisw <514335188@qq.com>
2020-12-30 13:15:22 +0800

    add filter to imu node.

--- src/imu/imu/node.py
+++ src/imu/imu/node.py
@@ -10,7 +10,6 @@ from tf2_ros.transform_broadcaster import TransformBroadcaster
 from gy85.adxl345.i2c import ADXL345
 from gy85.hmc5883l.HMC5883L import HMC5883L
 from gy85.itg3200.ITG3200 import ITG3200
-from complementary_filter.complementary_filter import ComplementaryFilter
 
 
 class IMUPublisher(Node):
@@ -27,27 +26,11 @@ class IMUPublisher(Node):
         self.declare_parameter('hmc5883l.addr')
         self.declare_parameter('hmc5883l.gauss')
         self.declare_parameter('hmc5883l.declination')
-        orientation_stddev = self.declare_parameter('filter.orientation_stddev', 0.0).get_parameter_value().double_value
-        do_bias_estimation = self.declare_parameter('filter.do_bias_estimation', True).get_parameter_value().bool_value
-        do_adaptive_gain = self.declare_parameter('filter.do_adaptive_gain', True).get_parameter_value().bool_value
-        gain_acc = self.declare_parameter('filter.gain_acc', 0.01).get_parameter_value().double_value
-        gain_mag = self.declare_parameter('filter.gain_mag', 0.01).get_parameter_value().double_value
-        bias_alpha = self.declare_parameter('filter.bias_alpha', 0.01).get_parameter_value().double_value
-        self.constant_dt = self.declare_parameter('filter.constant_dt', 0.02).get_parameter_value().double_value
+        self.dt = self.declare_parameter('dt').get_parameter_value().double_value
 
-        self.filter = ComplementaryFilter()
-        self.orientation_variance = orientation_stddev ** 2
-        self.filter.setDoBiasEstimation(do_bias_estimation)
-        self.filter.setDoAdaptiveGain(do_adaptive_gain)
-        if not self.filter.setGainAcc(gain_acc):
-            self.get_logger().warn('Invalid gain_acc passed to ComplementaryFilter.')
-        if not self.filter.setGainMag(gain_mag):
-            self.get_logger().warn('Invalid gain_mag passed to ComplementaryFilter.')
-        if do_bias_estimation:
-            if not self.filter.setBiasAlpha(bias_alpha):
-                self.get_logger().warn("Invalid bias_alpha passed to ComplementaryFilter.")
+        self.beta = math.sqrt(3 / 4) * math.pi * (10 / 180)
+        self.orientation = Quaternion(w=1)
 
-        self.initialized_filter = False
         self.smbus = self.get_parameter('smbus').get_parameter_value().integer_value
 
         self.accelerometer = ADXL345(self.smbus, self.get_parameter('adxl345.addr').get_parameter_value().integer_value)
@@ -63,7 +46,7 @@ class IMUPublisher(Node):
 
         self.imu_publisher = self.create_publisher(Imu, 'imu', QoSProfile(depth=100))
         self.tf_publisher = TransformBroadcaster(self)
-        self.timer = self.create_timer(self.constant_dt, self.poll)
+        self.timer = self.create_timer(self.dt, self.poll)
 
     def eular_to_quaternion(self, x, y, z):
         eular = [x * math.pi / 180 / 2, y * math.pi / 180 / 2, -z * math.pi / 180 / 2]
@@ -83,36 +66,20 @@ class IMUPublisher(Node):
     def poll(self):
         acc = self.accelerometer.read_data(scale=True)
         gyr = self.gyroscope.read_data()
-        # mag = self.compass.read_data()
-        # self.filter.update(*acc, *gyr, *mag, self.constant_dt)
-        #
-        # orientation = Quaternion()
-        # orientation.w, orientation.x, orientation.y, orientation.z = self.filter.getOrientation()
-        roll = math.atan2(acc[1], acc[2]) * 180 / math.pi
-        pitch = math.atan2(-acc[0], math.sqrt(acc[1] * acc[1] + acc[2] * acc[2])) * 180 / math.pi
-        yaw = self.compass.heading(pitch * math.pi / 180, roll * math.pi / 180)
+        mag = self.compass.read_data()
+
+        self.orientation = self.MadgwickQuaternionUpdate(*acc, *gyr, *mag)
+
+        # roll = math.atan2(acc[1], acc[2]) * 180 / math.pi
+        # pitch = math.atan2(-acc[0], math.sqrt(acc[1] * acc[1] + acc[2] * acc[2])) * 180 / math.pi
+        # yaw = self.compass.heading(pitch * math.pi / 180, roll * math.pi / 180)
 
         imu = Imu()
         imu.header.stamp = self.get_clock().now().to_msg()
         imu.header.frame_id = 'imu'
-        imu.orientation = self.eular_to_quaternion(roll, pitch, yaw)
+        imu.orientation = self.orientation
         imu.angular_velocity = Vector3(x=gyr[0], y=gyr[1], z=gyr[2])
         imu.linear_acceleration = Vector3(x=acc[0], y=acc[1], z=acc[2])
-        imu.orientation_covariance[0] = self.orientation_variance
-        imu.orientation_covariance[1] = 0.0
-        imu.orientation_covariance[2] = 0.0
-        imu.orientation_covariance[3] = 0.0
-        imu.orientation_covariance[4] = self.orientation_variance
-        imu.orientation_covariance[5] = 0.0
-        imu.orientation_covariance[6] = 0.0
-        imu.orientation_covariance[7] = 0.0
-        imu.orientation_covariance[8] = self.orientation_variance
-
-        # if self.filter.getDoBiasEstimation():
-        #     imu.angular_velocity.x -= self.filter.getAngularVelocityBiasX()
-        #     imu.angular_velocity.y -= self.filter.getAngularVelocityBiasY()
-        #     imu.angular_velocity.z -= self.filter.getAngularVelocityBiasZ()
-
         self.imu_publisher.publish(imu)
 
         tf = TransformStamped()
@@ -122,6 +89,82 @@ class IMUPublisher(Node):
         tf.header.frame_id = 'base_link'
         tf.child_frame_id = 'imu'
         self.tf_publisher.sendTransform(tf)
+
+    def MadgwickQuaternionUpdate(self, ax, ay, az, gx, gy, gz, mx, my, mz):
+        q1, q2, q3, q4 = self.orientation.w, self.orientation.x, self.orientation.y, self.orientation.z
+
+        # Auxiliary variables to avoid repeated arithmetic
+        _2q1 = 2.0 * q1
+        _2q2 = 2.0 * q2
+        _2q3 = 2.0 * q3
+        _2q4 = 2.0 * q4
+        _2q1q3 = 2.0 * q1 * q3
+        _2q3q4 = 2.0 * q3 * q4
+        q1q1 = q1 * q1
+        q1q2 = q1 * q2
+        q1q3 = q1 * q3
+        q1q4 = q1 * q4
+        q2q2 = q2 * q2
+        q2q3 = q2 * q3
+        q2q4 = q2 * q4
+        q3q3 = q3 * q3
+        q3q4 = q3 * q4
+        q4q4 = q4 * q4
+
+        # Normalise accelerometer measurement
+        norm = math.sqrt(ax * ax + ay * ay + az * az)
+        if norm == 0.0:  # handle NaN
+            return self.orientation
+        norm = 1.0 / norm
+        ax *= norm
+        ay *= norm
+        az *= norm
+
+        # Normalise magnetometer measurement
+        norm = math.sqrt(mx * mx + my * my + mz * mz)
+        if norm == 0.0:  # handle NaN
+            return self.orientation
+        norm = 1.0 / norm
+        mx *= norm
+        my *= norm
+        mz *= norm
+
+        # Reference direction of Earth's magnetic field
+        _2q1mx = 2.0 * q1 * mx
+        _2q1my = 2.0 * q1 * my
+        _2q1mz = 2.0 * q1 * mz
+        _2q2mx = 2.0 * q2 * mx
+        hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4
+        hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4
+        _2bx = math.sqrt(hx * hx + hy * hy)
+        _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4
+        _4bx = 2.0 * _2bx
+        _4bz = 2.0 * _2bz
+
+        # Gradient decent algorithm corrective step
+        s1 = -_2q3 * (2.0 * q2q4 - _2q1q3 - ax) + _2q2 * (2.0 * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5 - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5 - q2q2 - q3q3) - mz)
+        s2 = _2q4 * (2.0 * q2q4 - _2q1q3 - ax) + _2q1 * (2.0 * q1q2 + _2q3q4 - ay) - 4.0 * q2 * (1.0 - 2.0 * q2q2 - 2.0 * q3q3 - az) + _2bz * q4 * (_2bx * (0.5 - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5 - q2q2 - q3q3) - mz)
+        s3 = -_2q1 * (2.0 * q2q4 - _2q1q3 - ax) + _2q4 * (2.0 * q1q2 + _2q3q4 - ay) - 4.0 * q3 * (1.0 - 2.0 * q2q2 - 2.0 * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5 - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5 - q2q2 - q3q3) - mz)
+        s4 = _2q2 * (2.0 * q2q4 - _2q1q3 - ax) + _2q3 * (2.0 * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5 - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5 - q2q2 - q3q3) - mz)
+        norm = math.sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4)  # normalise step magnitude
+        norm = 1.0 / norm
+        s1 *= norm
+        s2 *= norm
+        s3 *= norm
+        s4 *= norm
+
+        qDot1 = 0.5 * (-q2 * gx - q3 * gy - q4 * gz) - self.beta * s1
+        qDot2 = 0.5 * (q1 * gx + q3 * gz - q4 * gy) - self.beta * s2
+        qDot3 = 0.5 * (q1 * gy - q2 * gz + q4 * gx) - self.beta * s3
+        qDot4 = 0.5 * (q1 * gz + q2 * gy - q3 * gx) - self.beta * s4
+
+        q1 += qDot1 * self.dt
+        q2 += qDot2 * self.dt
+        q3 += qDot3 * self.dt
+        q4 += qDot4 * self.dt
+        norm = math.sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4)
+        norm = 1.0 / norm
+        return Quaternion(w=q1 * norm, x=q2 * norm, y=q3 * norm, z=q4 * norm)
 
 
 def main(args=None):
